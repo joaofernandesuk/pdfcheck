8c2706eb58afc01f06d67e6e053852fb92378d08
Jason Morris <jasonmorris@gmail.com>
2017-02-24 14:54:26 -0500

    doubled results at the moment

--- pdfcheck.js
+++ pdfcheck.js
@@ -33,109 +33,125 @@ function supportsFileAPI() {
 function preUpload(event) {
 
 	// The file API supports the ability to reference multiple files in one <input> tag
-	var file = event.target.files[0];
-  var currentUpload = "upload-1";
-	var reader = new FileReader();
 
-	attachEvent(reader, "load", (function(fileToCheck) {
-		return function (evt) {
+  var uploadedFileNum = event.target.files.length;
+  var file;
+  var i;
+  var j;
+  var pdfName;
+  var reader;
 
-      var markup;
-      var reportDiv = document.createElement("div");
-      reportDiv.id = currentUpload;
-      reportDiv.className = "report-block";
+  document.getElementById("report").innerHTML = "";
 
-      document.getElementById("report").innerHTML = "";
-      document.getElementById("report").appendChild(reportDiv);
+  for (i = 0; i < uploadedFileNum; i++) {
+    file = event.target.files[i];
+    reader = new FileReader();
 
-      var dataFull = evt.target.result;
 
-      // Display file name
-      var fileName = file.name;
-      markup = "<strong>" + fileName + "</strong>";
-      createDiv(currentUpload, "title", markup);
+    attachEvent(reader, "load", (function(fileToCheck) {
 
-      // Check if valid PDF file (read first 8 bytes, match regex)
-      var dataHeader = dataFull.substr(0, 8);
-			var regexHeader = /%PDF-(1.[0-7])/g;
-      var matchHeader = regexHeader.exec(dataHeader);
-      if (!matchHeader) {
-        markup = "Not a valid PDF file.";
-        createDiv(currentUpload, "failure", markup);
-        return;
-      }
-      else {
-        markup = "<span>PDF Version:</span> <strong>" + matchHeader[1] + "</strong>";
-        createDiv(currentUpload, "default", markup);
-      }
+  		return function (evt) {
 
-      // Display file size
-      var KBSize = Math.ceil(file.size / 1024);
-      markup = "<span>File size:</span> <strong>" + KBSize + " KB</strong>";
-      createDiv(currentUpload, "default", markup);
+        for (j = 0; j < uploadedFileNum; j++) {
 
-      // Check if Lang is set, display value if set`
-      var regexLang = /Lang\((\S*)\)/g;
-      var matchLang = regexLang.exec(dataFull);
-      if (!!matchLang) {
-        markup = "<span>Language:</span> <strong>" + matchLang[1] + "</strong>";
-        createDiv(currentUpload, "success", markup);
-      }
-      else {
-        markup = "Language not set";
-        createDiv(currentUpload, "failure", markup);
-      }
+          var dataFull = evt.target.result;
 
-      // Check MarkInfo exists and whether true or false
-      var regexMarked = /\/MarkInfo\<\<\/Marked (true|false)/g;
-      var matchMarked = regexMarked.exec(dataFull);
-      if (!!matchMarked) {
-        if (matchMarked[1] == "true") {
-          markup = "<span>Marked:</span> <strong>True</strong>";
-          createDiv(currentUpload, "success", markup);
+          // Display file name
+          //pdfName = evt.target.file.name;
+
+            pdfName = event.target.files[j].name;
+            markup = "<strong>" + pdfName + "</strong>";
+            createDiv("report", "title", markup);
+          // pdfName = "hi";
+          // console.log(event.target.files);
+          // console.log(i);
+          // console.log(file);
+          // markup = "<strong>" + pdfName + "</strong>";
+          // createDiv("report", "title", markup);
+
+          // Check if valid PDF file (read first 8 bytes, match regex)
+          var dataHeader = dataFull.substr(0, 8);
+    			var regexHeader = /%PDF-(1.[0-7])/g;
+          var matchHeader = regexHeader.exec(dataHeader);
+          if (!matchHeader) {
+            markup = "Not a valid PDF file.";
+            createDiv("report", "failure", markup);
+            return;
+          }
+          else {
+            markup = "<span>PDF Version:</span> <strong>" + matchHeader[1] + "</strong>";
+            createDiv("report", "default", markup);
+          }
+
+          // Display file size
+          var KBSize = Math.ceil(file.size / 1024);
+          markup = "<span>File size:</span> <strong>" + KBSize + " KB</strong>";
+          createDiv("report", "default", markup);
+
+          // Check if Lang is set, display value if set`
+          var regexLang = /Lang\((\S*)\)/g;
+          var matchLang = regexLang.exec(dataFull);
+          if (!!matchLang) {
+            markup = "<span>Language:</span> <strong>" + matchLang[1] + "</strong>";
+            createDiv("report", "success", markup);
+          }
+          else {
+            markup = "Language not set";
+            createDiv("report", "failure", markup);
+          }
+
+          // Check MarkInfo exists and whether true or false
+          var regexMarked = /\/MarkInfo\<\<\/Marked (true|false)/g;
+          var matchMarked = regexMarked.exec(dataFull);
+          if (!!matchMarked) {
+            if (matchMarked[1] == "true") {
+              markup = "<span>Marked:</span> <strong>True</strong>";
+              createDiv("report", "success", markup);
+            }
+            else {
+              markup = "<span>Marked:</span> <strong>False</strong>";
+              createDiv("report", "warning", markup);
+            }
+          }
+          else {
+            markup = "Not marked";
+            createDiv("report", "failure", markup);
+          }
+
+          // Check if StructTreeRoot is set
+          var regexTree = /StructTreeRoot\s(\d*)\s(\d*)/g;
+          var matchTree = regexTree.exec(dataFull);
+          if (!!matchTree) {
+            markup = "<span>Tagged:</span> <strong>Yes (" + matchTree[1] + " tags)</strong>";
+            createDiv("report", "success", markup);
+          }
+          else {
+            markup = "Not tagged";
+            createDiv("report", "failure", markup);
+          }
+
+          // Check DisplayDocTitle exists and whether true or false
+          var regexTitle = /\/DisplayDocTitle (true|false)/g;
+          var matchTitle = regexTitle.exec(dataFull);
+          if (!!matchTitle) {
+            if (matchTitle[1] == "true") {
+              markup = "<span>Display Doc Title:</span> <strong>True</strong>";
+              createDiv("report", "success", markup);
+            }
+            else {
+              markup = "<span>Display Doc Title:</span> <strong>False</strong>";
+              createDiv("report", "warning", markup);
+            }
+          }
+          else {
+            markup = "Display Document Title not set";
+            createDiv("report", "failure", markup);
+          }
         }
-        else {
-          markup = "<span>Marked:</span> <strong>False</strong>";
-          createDiv(currentUpload, "warning", markup);
-        }
-      }
-      else {
-        markup = "Not marked";
-        createDiv(currentUpload, "failure", markup);
-      }
-
-      // Check if StructTreeRoot is set
-      var regexTree = /StructTreeRoot\s(\d*)\s(\d*)/g;
-      var matchTree = regexTree.exec(dataFull);
-      if (!!matchTree) {
-        markup = "<span>Tagged:</span> <strong>Yes (" + matchTree[1] + " tags)</strong>";
-        createDiv(currentUpload, "success", markup);
-      }
-      else {
-        markup = "Not tagged";
-        createDiv(currentUpload, "failure", markup);
-      }
-
-      // Check DisplayDocTitle exists and whether true or false
-      var regexTitle = /\/DisplayDocTitle (true|false)/g;
-      var matchTitle = regexTitle.exec(dataFull);
-      if (!!matchTitle) {
-        if (matchTitle[1] == "true") {
-          markup = "<span>Display Doc Title:</span> <strong>True</strong>";
-          createDiv(currentUpload, "success", markup);
-        }
-        else {
-          markup = "<span>Display Doc Title:</span> <strong>False</strong>";
-          createDiv(currentUpload, "warning", markup);
-        }
-      }
-      else {
-        markup = "Display Document Title not set";
-        createDiv(currentUpload, "failure", markup);
-      }
-		};
-	})(file));
-	reader.readAsText(file);
+  		};
+  	})(file));
+  	reader.readAsText(file);
+  }
 }
 
 function createDiv(parent, className, innerHTML) {
