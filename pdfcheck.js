500b8e5695fb1e7b0511ba5c0efbe9b215a38123
Jason Morris <jasonmorris@gmail.com>
2017-02-23 23:14:15 -0500

    Prepare for looping through multiple files

--- pdfcheck.js
+++ pdfcheck.js
@@ -14,7 +14,7 @@ function attachEvent(element, event, callbackFunction) {
 		element.addEventListener(event, callbackFunction, false);
 	}
 	else if(element.attachEvent)  {
-		element.attachEvent('on' + event, callbackFunction);
+		element.attachEvent("on" + event, callbackFunction);
 	}
 }
 
@@ -31,51 +31,59 @@ function supportsFileAPI() {
  * @param event
  */
 function preUpload(event) {
-  // clear existing fields
-  document.getElementById("pdfStatus").innerHTML = "";
-  document.getElementById("pdfVersion").innerHTML = "";
-  document.getElementById("pdfSize").innerHTML = "";
-  document.getElementById("pdfTagged").innerHTML = "";
-  document.getElementById("pdfMarked").innerHTML = "";
-  document.getElementById("pdfLang").innerHTML = "";
-  document.getElementById("pdfTitle").innerHTML = "";
 
 	// The file API supports the ability to reference multiple files in one <input> tag
 	var file = event.target.files[0];
+  var currentUpload = "upload-1";
 	var reader = new FileReader();
 
 	attachEvent(reader, "load", (function(fileToCheck) {
 		return function (evt) {
+
+      var markup;
+      var reportDiv = document.createElement("div");
+      reportDiv.id = currentUpload;
+      reportDiv.className = "report-block";
+
+      document.getElementById("report").innerHTML = "";
+      document.getElementById("report").appendChild(reportDiv);
+
       var dataFull = evt.target.result;
+
+      // Display file name
+      var fileName = file.name;
+      markup = "<strong>" + fileName + "</strong>";
+      createDiv(currentUpload, "title", markup);
 
       // Check if valid PDF file (read first 8 bytes, match regex)
       var dataHeader = dataFull.substr(0, 8);
 			var regexHeader = /%PDF-(1.[0-7])/g;
       var matchHeader = regexHeader.exec(dataHeader);
       if (!matchHeader) {
-        document.getElementById("pdfStatus").innerHTML = "<div class='failure'>Not a valid PDF file.</div>";
+        markup = "Not a valid PDF file.";
+        createDiv(currentUpload, "failure", markup);
         return;
       }
       else {
-        document.getElementById("pdfVersion").innerHTML = "<div class='default'><span>PDF Version:</span> <strong>" + matchHeader[1] + "</strong></div>";
+        markup = "<span>PDF Version:</span> <strong>" + matchHeader[1] + "</strong>";
+        createDiv(currentUpload, "default", markup);
       }
-
-      // Display file name
-      var fileName = file.name;
-      document.getElementById("pdfName").innerHTML = "<strong>" + fileName + "</strong>";
 
       // Display file size
       var KBSize = Math.ceil(file.size / 1024);
-      document.getElementById("pdfSize").innerHTML = "<div class='default'><span>File size:</span> <strong>" + KBSize + " KB</strong></div>";
+      markup = "<span>File size:</span> <strong>" + KBSize + " KB</strong>";
+      createDiv(currentUpload, "default", markup);
 
       // Check if Lang is set, display value if set`
       var regexLang = /Lang\((\S*)\)/g;
       var matchLang = regexLang.exec(dataFull);
       if (!!matchLang) {
-        document.getElementById("pdfLang").innerHTML = "<div class='success'><span>Language:</span> <strong>" + matchLang[1] + "</strong></div>";
+        markup = "<span>Language:</span> <strong>" + matchLang[1] + "</strong>";
+        createDiv(currentUpload, "success", markup);
       }
       else {
-        document.getElementById("pdfLang").innerHTML = "<div class='failure'>Language not set</div>";
+        markup = "Language not set";
+        createDiv(currentUpload, "failure", markup);
       }
 
       // Check MarkInfo exists and whether true or false
@@ -83,24 +91,29 @@ function preUpload(event) {
       var matchMarked = regexMarked.exec(dataFull);
       if (!!matchMarked) {
         if (matchMarked[1] == "true") {
-          document.getElementById("pdfMarked").innerHTML = "<div class='success'><span>Marked:</span> <strong>True</strong></div>";
+          markup = "<span>Marked:</span> <strong>True</strong>";
+          createDiv(currentUpload, "success", markup);
         }
         else {
-          document.getElementById("pdfMarked").innerHTML = "<div class='warning'><span>Marked:</span> <strong>False</strong></div>";
+          markup = "<span>Marked:</span> <strong>False</strong>";
+          createDiv(currentUpload, "warning", markup);
         }
       }
       else {
-        document.getElementById("pdfMarked").innerHTML = "<div class='failure'>Not marked</div>";
+        markup = "Not marked";
+        createDiv(currentUpload, "failure", markup);
       }
 
       // Check if StructTreeRoot is set
       var regexTree = /StructTreeRoot\s(\d*)\s(\d*)/g;
       var matchTree = regexTree.exec(dataFull);
       if (!!matchTree) {
-        document.getElementById("pdfTagged").innerHTML = "<div class='success'><span>Tagged:</span> <strong>Yes (" + matchTree[1] + " tags)</strong></div>";
+        markup = "<span>Tagged:</span> <strong>Yes (" + matchTree[1] + " tags)</strong>";
+        createDiv(currentUpload, "success", markup);
       }
       else {
-        document.getElementById("pdfTagged").innerHTML = "<div class='failure'>Not tagged</div>";
+        markup = "Not tagged";
+        createDiv(currentUpload, "failure", markup);
       }
 
       // Check DisplayDocTitle exists and whether true or false
@@ -108,18 +121,28 @@ function preUpload(event) {
       var matchTitle = regexTitle.exec(dataFull);
       if (!!matchTitle) {
         if (matchTitle[1] == "true") {
-          document.getElementById("pdfTitle").innerHTML = "<div class='success'><span>Display Doc Title:</span> <strong>True</strong></div>";
+          markup = "<span>Display Doc Title:</span> <strong>True</strong>";
+          createDiv(currentUpload, "success", markup);
         }
         else {
-          document.getElementById("pdfTitle").innerHTML = "<div class='warning'><span>Display Doc Title:</span> <strong>False</strong></div>";
+          markup = "<span>Display Doc Title:</span> <strong>False</strong>";
+          createDiv(currentUpload, "warning", markup);
         }
       }
       else {
-        document.getElementById("pdfTitle").innerHTML = "<div class='failure'>Display Document Title not set</div>";
+        markup = "Display Document Title not set";
+        createDiv(currentUpload, "failure", markup);
       }
 		};
 	})(file));
 	reader.readAsText(file);
+}
+
+function createDiv(parent, className, innerHTML) {
+  var tempNode = document.createElement("div");
+  tempNode.className = className;
+  tempNode.innerHTML = innerHTML;
+  document.getElementById(parent).appendChild(tempNode);
 }
 
 function pageLoaded() {
